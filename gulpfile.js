31b33c8fbf482e481f5fd0df582c85dd2f226448
Gadi <gdishim@gmail.com>
2017-09-06 04:36:07 +0300

    added sass

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass')
+    notify = require('gulp-notify')
+    bower = require('gulp-bower');
+
+var config = {
+  sassPath: './sass',
+  bowerDir:'./bower_components'
+}
+gulp.task('bower', function(){
+  return bower()
+          .pipe(gulp.dest(config.bowerDir))
+});
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/font-awesome/fonts/**.*')
+        .pipe(gulp.dest('./public/fonts'));
+});
+gulp.task('css', function() {
+    return sass(config.sassPath + '/app.scss',
+      {
+          style: 'compressed',
+          loadPath: [
+              './sass',
+              config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+              config.bowerDir + '/font-awesome/scss',
+          ]
+      }).on("error", notify.onError(function (error) {
+              return "Error: " + error.message;
+          })).pipe(gulp.dest('./public/css'));
+});
+gulp.task('watch', function() {
+  gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['bower', 'icons', 'css']);
